f0080610b0045ed048c3ba79fc8d75982135ace9
kalvin <kalvin.vasconcellos@gmail.com>
2020-09-13 03:15:14 -0300

    infra: add continuous delivery

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,31 @@
- #!/bin/sh
+ #!/bin/bash
 
 FRONTEND=fortunator-frontend
 BACKEND=fortunator-backend
 
-# BACKEND
-heroku container:push web --app=$BACKEND
-
-heroku container:release web --app=$BACKEND
+IS_BACKEND=$(git log --no-merges --name-only -p -2 origin/master | cut -d/ -f1 | grep back | head -1)
+IS_FRONTEND=$(git log --no-merges --name-only -p -2 origin/master | cut -d/ -f1 | grep front | head -1)
 
 
-# FRONTEND
-heroku container:push web --app=$FRONTEND
+if [[ "$IS_BACKEND" = "backend" ]]  ; then
+    echo "Starting backend deploy..."
 
-heroku container:release web --app=$FRONTEND
+    cd backend
+    
+    heroku container:push web --app=$BACKEND
+
+    heroku container:release web --app=$BACKEND
+    
+
+elif [[ "$IS_FRONTEND" = "frontend" ]]  ; then
+    echo "Starting frontend deploy..."
+
+    cd frontend
+
+    heroku container:push web --app=$FRONTEND
+
+    heroku container:release web --app=$FRONTEND
+
+fi
+
 
