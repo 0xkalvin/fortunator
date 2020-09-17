ba29fbe9709e2b3f5010ceab534df66a10a21028
kalvin <kalvin.vasconcellos@gmail.com>
2020-09-16 21:53:12 -0300

    backend: fix health test adding class context

--- backend/src/test/java/com/fortunator/api/controller/HealthControllerTest.java
+++ backend/src/test/java/com/fortunator/api/controller/HealthControllerTest.java
@@ -9,7 +9,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-@WebMvcTest
+
+@WebMvcTest(HealthController.class)
 public class HealthControllerTest {
 
 	@Autowired
