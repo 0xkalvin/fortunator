56b50b4852ca4323af7769648071ada6ea961049
Kaique <kaiquejuvencio@hotmail.com>
2020-11-01 16:45:03 -0300

    front: adjusts css home

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -25,9 +25,7 @@ export default function Home() {
                     color += letters[Math.floor(Math.random() * 16)];          
                 } 
                 randomColorsArray.push(color);   
-            }
-            
-              
+            }        
             return randomColorsArray;
           }
         
@@ -45,7 +43,7 @@ export default function Home() {
                         setTeste1(spendAmountArray);
                     });
                 } catch (err) {
-                    alert(err);
+                    alert("Algo deu errado :(");
                 }
             }, []) // <-- empty dependency array
             return <div></div>
@@ -54,34 +52,37 @@ export default function Home() {
         return (
         <div className="home-container"> 
             <Hamburguer/>
-            <div className="div-logo">
+            <div className="div-logo" style={{paddingLeft: "16%"}}>
                 <Logo />
                 <div className="div-logo-description">
                     <h2>Fortunator</h2>
                     <h3>Controle Financeiro</h3>
                 </div>
             </div>
-            <div className="div-gif">
-                <img src={finger} height="55px" alt="finger-gif" />
-                <div>
-                     <h1 className="title-gif">Olá</h1>
-                     <p className="sub-title">Bem-Vindo ao Fortunator.</p>
-                </div>     
+            <div className="body-home">
+                <div className="div-gif">
+                    <img src={finger} height="55px" alt="finger-gif" />
+                    <div>
+                        <h1 className="title-gif">Olá</h1>
+                        <p className="sub-title">Bem-Vindo ao Fortunator.</p>
+                    </div>     
+                </div>
+                
+                <Chart chartType='Bar' chartDataLabels={['Janeiro', 'Feveireiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']}  chartDataData={[617594,581045,453060,617594,581045,453060,617594,581045,453060,617594,581045,453060,617594,581045,453060]} chartDataColor={['rgba(255, 99, 132, 0.6)','rgba(54, 162, 235, 0.6)', 'rgba(255, 206, 86, 0.6)','rgba(255, 99, 132, 0.6)','rgba(54, 162, 235, 0.6)', 'rgba(255, 206, 86, 0.6)','rgba(255, 99, 132, 0.6)','rgba(54, 162, 235, 0.6)', 'rgba(255, 206, 86, 0.6)','rgba(255, 99, 132, 0.6)','rgba(54, 162, 235, 0.6)', 'rgba(255, 206, 86, 0.6)','rgba(255, 99, 132, 0.6)','rgba(54, 162, 235, 0.6)', 'rgba(255, 206, 86, 0.6)']} legendPosition="bottom" textTitle='Gasto Mensal em 2020' />
+                <select description="Tipo do Gráfico" id="Transac" className="select-chart-type" onChange={e => {setChartType(e.target.value)}}>
+                                <option value="Pie">Pizza</option>
+                                <option value="Bar">Barra</option>
+                                <option value="Line">Linha</option>
+                </select>
+                {(function () {
+                    if(categorySpendAmount.length !== 0){
+                        console.log(getRandomColor(categorySpendAmount.length));
+                        return(    
+                            <Chart chartType={chartType} chartDataLabels={categoryNames}  chartDataData={categorySpendAmount} chartDataColor={getRandomColor(categorySpendAmount.length)} legendPosition="bottom" textTitle='Gasto Mensal Por Categoria' />         
+                        )
+                    }     
+                })()}
             </div>
-            <Chart chartType='Bar' chartDataLabels={['Janeiro', 'Feveireiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']}  chartDataData={[617594,581045,453060,617594,581045,453060,617594,581045,453060,617594,581045,453060,617594,581045,453060]} chartDataColor={['rgba(255, 99, 132, 0.6)','rgba(54, 162, 235, 0.6)', 'rgba(255, 206, 86, 0.6)','rgba(255, 99, 132, 0.6)','rgba(54, 162, 235, 0.6)', 'rgba(255, 206, 86, 0.6)','rgba(255, 99, 132, 0.6)','rgba(54, 162, 235, 0.6)', 'rgba(255, 206, 86, 0.6)','rgba(255, 99, 132, 0.6)','rgba(54, 162, 235, 0.6)', 'rgba(255, 206, 86, 0.6)','rgba(255, 99, 132, 0.6)','rgba(54, 162, 235, 0.6)', 'rgba(255, 206, 86, 0.6)']} legendPosition="bottom" textTitle='Gasto Mensal em 2020' />
-            <select description="Tipo do Gráfico" id="Transac" className="select-chart-type" onChange={e => {setChartType(e.target.value)}}>
-                            <option value="Pie">Pizza</option>
-                            <option value="Bar">Barra</option>
-                            <option value="Line">Linha</option>
-            </select>
-            {(function () {
-                if(categorySpendAmount.length !== 0){
-                    console.log(getRandomColor(categorySpendAmount.length));
-                    return(    
-                        <Chart chartType={chartType} chartDataLabels={categoryNames}  chartDataData={categorySpendAmount} chartDataColor={getRandomColor(categorySpendAmount.length)} legendPosition="bottom" textTitle='Gasto Mensal Por Categoria' />         
-                    )
-                }     
-            })()}
             </div>
         )
 
