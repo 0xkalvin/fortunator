abd66860f7b31121c61ffd10853e8623817f1aac
kalvin <kalvin.vasconcellos@gmail.com>
2020-11-03 23:33:09 -0300

    backend: add score and level columns to user and create goals table

--- backend/src/main/resources/db/migration/V006__create-goals.sql
+++ backend/src/main/resources/db/migration/V006__create-goals.sql
@@ -1,15 +1,14 @@
-CREATE TYPE goals_types AS ENUM ('budget');
-
 CREATE TABLE goals (
-	id INT GENERATED ALWAYS AS IDENTITY,
 	amount decimal(10,2),
-	created_at date NOT NULL,
+	date date NOT NULL,
 	description varchar(255) NOT NULL,
-	type goals_types NOT NULL,
+	id INT GENERATED ALWAYS AS IDENTITY,
+	status varchar(50) NOT NULL,
+	score decimal(10,2) NOT NULL,
+	type varchar(50) NOT NULL,
 	user_id INT NOT NULL,
 
-	primary key (id),
-	UNIQUE(user_id)
+	primary key (id)
 );
 
-alter table goals add constraint fk_user foreign key (user_id) references users(id);
+alter table goals add constraint fk_user foreign key (user_id) references users (id);
