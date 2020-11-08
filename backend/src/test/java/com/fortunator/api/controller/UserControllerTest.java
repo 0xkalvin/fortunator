917f4ad525c0f063d4710005aed6a30edc53c1a7
kalvin <kalvin.vasconcellos@gmail.com>
2020-11-08 18:27:59 -0300

    backend: add endpoint for getting one user by id

--- backend/src/test/java/com/fortunator/api/controller/UserControllerTest.java
+++ backend/src/test/java/com/fortunator/api/controller/UserControllerTest.java
@@ -2,6 +2,7 @@ package com.fortunator.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.http.MediaType;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -10,7 +11,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
 
 import com.fortunator.api.models.User;
 import com.fortunator.api.service.*;
@@ -26,20 +26,26 @@ public class UserControllerTest {
 
 	@Test
 	public void shouldCreateUserSuccessfully() throws Exception {
+		MockHttpServletRequestBuilder request = post("/users").contentType(MediaType.APPLICATION_JSON)
+				.content(asJsonString(new User(null, "kaique", "kaique@boladao.com", "senhadokaique123")));
 
-		this.mockMvc
-				.perform(post("/users").contentType(MediaType.APPLICATION_JSON).content(
-						asJsonString(new User(null, "kaique", "kaique@boladao.com", "senhadokaique123"))))
-				.andExpect(status().isCreated());
+		this.mockMvc.perform(request).andExpect(status().isCreated());
 	}
 
 	@Test
 	public void shouldNotCreateDueToInvalidEmail() throws Exception {
+		MockHttpServletRequestBuilder request = post("/users").contentType(MediaType.APPLICATION_JSON)
+				.content(asJsonString(new User(null, "kaique", "email", "senhadokaique123")));
 
-		this.mockMvc
-				.perform(post("/users").contentType(MediaType.APPLICATION_JSON)
-						.content(asJsonString(new User(null, "kaique", "email", "senhadokaique123"))))
-				.andExpect(status().isBadRequest());
+		this.mockMvc.perform(request).andExpect(status().isBadRequest());
+	}
+
+	@Test
+	public void shouldGetUserById() throws Exception {
+
+		MockHttpServletRequestBuilder request = get("/users/1").contentType(MediaType.APPLICATION_JSON);
+
+		this.mockMvc.perform(request).andExpect(status().isOk());
 	}
 
 	public static String asJsonString(final Object obj) {
